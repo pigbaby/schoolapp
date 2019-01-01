93490d26f1948f34daf3b455a1a96fa9270f88f6
pigbaby <pigbabyzizi@gmail.com>
2019-01-01 23:05:49 +0800

    finish the framework of gradetest

--- routes/myrouter.js
+++ routes/myrouter.js
@@ -6,9 +6,13 @@ router.get('/',function(req,res,next){
     //res.sendFile('bootStrap');
 });
 
-// This is a router for test page.
+// This is a router for jquery test page.
 router.get('/test',function(req,res,next){
     res.render('jquerytest');
 });
 
+// This is a router for grade test page.
+router.get('/gradetest',function(req,res,next){
+    res.render('gradetest');
+});
 module.exports=router;
